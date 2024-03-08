95704e331f92de4bce5cd383fecf557177ac6bc2
Mauro Busso <mauro.busso12@gmail.com>
2024-03-08 20:59:20 +0000

    fixed minor style issues

--- pubblic/js/script.js
+++ pubblic/js/script.js
@@ -83,14 +83,6 @@ async function deleteMedication(id) {
 
 // }
 
-// async function calculateDosage(){
-//   console.log('calculated dosage')
-//   document.getElementById("result").value = 58
-// }
-
-//toggle medications in item page
-// document.getElementById('resultButton').addEventListener('click', toggleMedications)
-
 //show form in medications.ejs
 function showForm(){
   document.getElementById('addMedication').removeAttribute('hidden')
