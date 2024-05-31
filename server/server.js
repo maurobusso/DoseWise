76d4d535b4a2f2e2c38caed468b5bc19ec3ba6cb
Mauro Busso <mauro.busso12@gmail.com>
2024-05-31 21:11:23 +0100

    fixing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const supabase = require('../config/supabaseConfig')
 const itemRoutes = require('../routes/itemRoutes')
 
