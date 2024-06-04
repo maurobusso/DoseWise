12e890ef0b40e0588391ad034e8fa6ec58ae24cb
Mauro Busso <mauro.busso12@gmail.com>
2024-06-04 20:45:58 +0100

    changed server settings

--- server/server.js
+++ server/server.js
@@ -29,7 +29,11 @@ supabase.from('items').select('id').range(0,0)
 app.use(cors())
 
 //serve static files 
-app.use(express.static('public'))
+//teh second line is an absolute path which is more verbose but More reliable, 
+//as it always resolves to the intended public directory regardless of the working directory. where the first one 
+//Can be error-prone if the server is started from a different directory.
+//app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 //help server parse requests for strings or array
